5f81de3591b98a0025216b4ef49d923d4b77d016
Zhiyuan <zyguochn@gmail.com>
2017-03-27 00:52:28 +0800

    Comment block and uncomment block feature supported

---
+++ NPL/Language/Editor/EditorCommandFilter.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+using System.Diagnostics;
+using System;
+using Microsoft.VisualStudio.Text.Editor;
+
+namespace NPLTools.Language.Editor
+{
+    internal class EditorCommandFilter : IOleCommandTarget
+    {
+        public EditorCommandFilter(IWpfTextView textView)
+        {
+            TextView = textView;
+        }
+
+        public IWpfTextView TextView { get; private set; }
+
+        public IOleCommandTarget Next { get; set; }
+
+        public int Exec(ref Guid pguidCmdGroup, uint nCmdID, uint nCmdexecopt, IntPtr pvaIn, IntPtr pvaOut)
+        {
+            if (pguidCmdGroup == VsMenus.guidStandardCommandSet2K)
+            {
+                switch ((VSConstants.VSStd2KCmdID)nCmdID)
+                {
+                    case VSConstants.VSStd2KCmdID.COMMENT_BLOCK:
+                    case VSConstants.VSStd2KCmdID.COMMENTBLOCK:
+                        CommentHelper.CommentOrUncommentBlock(TextView, true);
+                        break;
+                    case VSConstants.VSStd2KCmdID.UNCOMMENT_BLOCK:
+                    case VSConstants.VSStd2KCmdID.UNCOMMENTBLOCK:
+                        CommentHelper.CommentOrUncommentBlock(TextView, false);
+                        break;
+                    case VSConstants.VSStd2KCmdID.FORMATSELECTION:
+                        Debug.WriteLine("Format Selection Command");
+                        break;
+                }
+            }
+
+            return Next.Exec(ref pguidCmdGroup, nCmdID, nCmdexecopt, pvaIn, pvaOut);
+        }
+
+        public int QueryStatus(ref Guid pguidCmdGroup, uint cCmds, OLECMD[] prgCmds, IntPtr pCmdText)
+        {
+            if (pguidCmdGroup == VsMenus.guidStandardCommandSet2K)
+            {
+                for (int i = 0; i < cCmds; i++)
+                {
+                    switch ((VSConstants.VSStd2KCmdID)prgCmds[i].cmdID)
+                    {
+                        case VSConstants.VSStd2KCmdID.COMMENT_BLOCK:
+                        case VSConstants.VSStd2KCmdID.COMMENTBLOCK:
+                        case VSConstants.VSStd2KCmdID.UNCOMMENT_BLOCK:
+                        case VSConstants.VSStd2KCmdID.UNCOMMENTBLOCK:
+                        case VSConstants.VSStd2KCmdID.FORMATSELECTION:
+                            prgCmds[i].cmdf = (uint)(OLECMDF.OLECMDF_ENABLED | OLECMDF.OLECMDF_SUPPORTED);
+                            return VSConstants.S_OK;
+                    }
+                }
+            }
+            return Next.QueryStatus(pguidCmdGroup, cCmds, prgCmds, pCmdText);
+        }
+    }
+}
--- NPL/Language/Editor/NPLTextViewCreationListener.cs
+++ NPL/Language/Editor/NPLTextViewCreationListener.cs
@@ -5,6 +5,8 @@ using System.ComponentModel.Composition;
 using Microsoft.VisualStudio.Utilities;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.OLE.Interop;
+using System.Runtime.InteropServices;
 
 namespace NPLTools.Language.Editor
 {
@@ -23,8 +25,19 @@ namespace NPLTools.Language.Editor
         public void VsTextViewCreated(IVsTextView textViewAdapter)
         {
             _view = AdaptersFactory.GetWpfTextView(textViewAdapter);
-
             _view.TextBuffer.Changed += TextBuffer_Changed;
+
+            IOleCommandTarget next;
+            EditorCommandFilter commandFilter = new EditorCommandFilter(_view);
+            textViewAdapter.AddCommandFilter(commandFilter, out next);
+            commandFilter.Next = next;
+
+            //IVsTextLines lines;
+            //textViewAdapter.GetBuffer(out lines);
+            //TextSpan[] spans = new TextSpan[1];
+            //lines.ReplaceLines(0, 0, 0, 0, Marshal.StringToHGlobalAnsi("hahaha"), 6, spans);
+            //TextBuffer tb = new TextBuffer();
+            //textViewAdapter.SetBuffer(lines);
         }
 
         private void TextBuffer_Changed(object sender, Microsoft.VisualStudio.Text.TextContentChangedEventArgs e)
