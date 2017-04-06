2c3950c65601f6b36aebe5680ad872cb96900742
Zhiyuan <zyguochn@gmail.com>
2017-04-06 11:12:43 +0800

    Add tooltip prototype

---
+++ NPL/Language/Tooltip/NPLQuickInfoController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.Language.Intellisense;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Utilities;
+
+namespace NPLTools.Language.Tooltip
+{
+    internal class NPLQuickInfoController : IIntellisenseController
+    {
+        private ITextView _textView;
+        private IList<ITextBuffer> _subjectBuffers;
+        private NPLQuickInfoControllerProvider _provider;
+        private IQuickInfoSession _session;
+
+        internal NPLQuickInfoController(ITextView textView, IList<ITextBuffer> subjectBuffers, NPLQuickInfoControllerProvider provider)
+        {
+            _textView = textView;
+            _subjectBuffers = subjectBuffers;
+            _provider = provider;
+
+            _textView.MouseHover += this.OnTextViewMouseHover;
+        }
+
+        private void OnTextViewMouseHover(object sender, MouseHoverEventArgs e)
+        {
+            SnapshotPoint? point = _textView.BufferGraph.MapDownToFirstMatch
+                (new SnapshotPoint(_textView.TextSnapshot, e.Position),
+                PointTrackingMode.Positive,
+                snapshot => _subjectBuffers.Contains(snapshot.TextBuffer),
+                PositionAffinity.Predecessor);
+
+            if (point != null)
+            {
+                ITrackingPoint triggerPoint = point.Value.Snapshot.CreateTrackingPoint(point.Value.Position, PointTrackingMode.Positive);
+
+                if (!_provider.QuickInfoBroker.IsQuickInfoActive(_textView))
+                {
+                    _session = _provider.QuickInfoBroker.TriggerQuickInfo(_textView, triggerPoint, true);
+                }
+            }
+        }
+
+        public void ConnectSubjectBuffer(ITextBuffer subjectBuffer)
+        {
+            
+        }
+
+        public void Detach(ITextView textView)
+        {
+            if (_textView == textView)
+            {
+                _textView.MouseHover -= this.OnTextViewMouseHover;
+                _textView = null;
+            }
+        }
+
+        public void DisconnectSubjectBuffer(ITextBuffer subjectBuffer)
+        {
+            
+        }
+    }
+
+    [Export(typeof(IIntellisenseControllerProvider))]
+    [Name("ToolTip QuickInfo Controller")]
+    [ContentType("NPL")]
+    internal class NPLQuickInfoControllerProvider : IIntellisenseControllerProvider
+    {
+        [Import]
+        internal IQuickInfoBroker QuickInfoBroker { get; set; }
+
+        public IIntellisenseController TryCreateIntellisenseController(ITextView textView, IList<ITextBuffer> subjectBuffers)
+        {
+            return new NPLQuickInfoController(textView, subjectBuffers, this);
+        }
+    }
+}
---
+++ NPL/Language/Tooltip/NPLQuickInfoSource.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Operations;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
+
+namespace NPLTools.Language.Tooltip
+{
+    internal class NPLQuickInfoSource : IQuickInfoSource
+    {
+        private NPLQuickInfoSourceProvider _provider;
+        private ITextBuffer _subjectBuffer;
+        private Dictionary<string, string> _dictionary;
+        private bool _isDisposed;
+
+        public NPLQuickInfoSource(NPLQuickInfoSourceProvider provider, ITextBuffer subjectBuffer)
+        {
+            _provider = provider;
+            _subjectBuffer = subjectBuffer;
+
+            _dictionary = new Dictionary<string, string>();
+            _dictionary.Add("add", "int add(int firstInt, int secondInt)\nAdds one integer to another.");
+            _dictionary.Add("subtract", "int subtract(int firstInt, int secondInt)\nSubtracts one integer from another.");
+            _dictionary.Add("multiply", "int multiply(int firstInt, int secondInt)\nMultiplies one integer by another.");
+            _dictionary.Add("divide", "int divide(int firstInt, int secondInt)\nDivides one integer by another.");
+        }
+
+        public void AugmentQuickInfoSession(IQuickInfoSession session, IList<object> quickInfoContent, out ITrackingSpan applicableToSpan)
+        {
+            SnapshotPoint? subjectTriggerPoint = session.GetTriggerPoint(_subjectBuffer.CurrentSnapshot);
+            if (!subjectTriggerPoint.HasValue)
+            {
+                applicableToSpan = null;
+                return;
+            }
+
+            ITextSnapshot currentSnapshot = subjectTriggerPoint.Value.Snapshot;
+            SnapshotSpan querySpan = new SnapshotSpan(subjectTriggerPoint.Value, 0);
+
+            ITextStructureNavigator navigator = _provider.NavigatorService.GetTextStructureNavigator(_subjectBuffer);
+            TextExtent extent = navigator.GetExtentOfWord(subjectTriggerPoint.Value);
+            string searchText = extent.Span.GetText();
+
+            foreach (string key in _dictionary.Keys)
+            {
+                int foundIndex = searchText.IndexOf(key, StringComparison.CurrentCultureIgnoreCase);
+                if (foundIndex > -1)
+                {
+                    applicableToSpan = currentSnapshot.CreateTrackingSpan(extent.Span.Start + foundIndex, key.Length, SpanTrackingMode.EdgeInclusive);
+
+                    string value;
+                    _dictionary.TryGetValue(key, out value);
+                    if (value != null)
+                        quickInfoContent.Add(value);
+                    else
+                        quickInfoContent.Add("");
+
+                    return;
+                }
+            }
+
+            applicableToSpan = null;
+        }
+
+        public void Dispose()
+        {
+            if (!_isDisposed)
+            {
+                GC.SuppressFinalize(this);
+                _isDisposed = true;
+            }
+        }
+    }
+
+
+    [Export(typeof(IQuickInfoSourceProvider))]
+    [Name("ToolTip QuickInfo Source")]
+    [Order(Before = "Default Quick Info Presenter")]
+    [ContentType("NPL")]
+    internal class NPLQuickInfoSourceProvider : IQuickInfoSourceProvider
+    {
+        [Import]
+        internal ITextStructureNavigatorSelectorService NavigatorService { get; set; }
+
+        [Import]
+        internal ITextBufferFactoryService TextBufferFactoryService { get; set; }
+
+        public IQuickInfoSource TryCreateQuickInfoSource(ITextBuffer textBuffer)
+        {
+            return new NPLQuickInfoSource(this, textBuffer);
+        }
+    }
+}
